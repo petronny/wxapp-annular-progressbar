cf9a8b00821796a14a45d0940068674775869911
laobingm <laobingm@qq.com>
2018-07-20 09:13:09 +0800

    optimization progress bar

--- index/index.js
+++ index/index.js
@@ -2,91 +2,115 @@ const app = getApp()
 
 Page({
   data: {
-    left_circle_transform: 'rotate(-135deg)',
-    left_circle_transition: 'all 0.5s linear',
-    right_circle_transform: 'rotate(-135deg)',
-    right_circle_transition: 'all 0.5s linear',
+    left_transform: -180,
+    left_transition: 0,
+    right_transform: -180,
+    right_transition: 0,
+    button_text: 'ANP',
   },
+
   /**
-   *设置进度条的变化
-   *@param {number} oldPercent    进度条改变之前的半分比
-   *@param {number} curPercent    进度条当前要设置的值 
+   * 传入进度条比例，自动计算动画过度角度和时间
+   * @param {number} target 进度条的比例，支持 0-100
    */
-  setProgressbar: function(oldPercent, curPercent) {
+  setProgressbar: function(target) {
+    let _this = this;
+    if (target > 100) target = target % 100
     let speed = 1; //改变50%进度所需的时间，单位秒   
     let time = 0; //进度条改变的时间
-    let deg = 0; //进度条改变的角度
-    let _this = this;
-    if (oldPercent > 50) { //原来进度大于50
-      if (curPercent > 50) {
-        //设置左边的进度
-        time = Math.abs(curPercent - oldPercent) / 50 * speed;
-        deg = curPercent / 50 * 180 - 135;
-        _this.setData({
-          left_circle_transform: 'rotate(' + deg + 'deg)',
-          left_circle_transition: 'all ' + time + 's linear'
-        })
-      } else {
-        //设置左边的进度
-        time = (oldPercent - 50) / 50 * speed;
-        deg = -135;
-        _this.setData({
-          left_circle_transform: 'rotate(' + deg + 'deg)',
-          left_circle_transition: 'all ' + time + 's linear'
-        })
-        //延时设置右边进度条的改变
-        setTimeout(function() {
-          time = (50 - curPercent) / 50;
-          deg = curPercent / 50 * 180 - 135;
-          _this.setData({
-            right_circle_transform: 'rotate(' + deg + 'deg)',
-            right_circle_transition: 'all ' + time + 's linear'
-          })
-        }, Math.floor(time * 1000));
-      }
-    } else { //原来进度小于50时
-      if (curPercent > 50) {
-        //设置右边的进度
-        time = (50 - oldPercent) / 50 * speed;
-        deg = 45;
-        _this.setData({
-          right_circle_transform: 'rotate(' + deg + 'deg)',
-          right_circle_transition: 'all ' + time + 's linear'
-        })
-        //延时设置左边进度条的改变
-        setTimeout(function() {
-          time = (curPercent - 50) / 50;
-          deg = (curPercent - 50) / 50 * 180 - 135;
+    // 计算历史数据
+    let old_left_deg = this.data.left_transform; //左半圆已转动的角度
+    let old_right_deg = this.data.right_transform; //右半圆已转动的角度
+    let remain_deg = 360 - (Math.abs(old_left_deg) + Math.abs(old_right_deg));
+    let old_percent = (remain_deg / 360) * 100;
+    //计算变化时间和角度
+    time = Math.abs(target - old_percent) / 50 * speed;
+    let new_deg = Math.abs(target) / 100 * 360;
+    // let change_deg = Math.abs(old_percent - target) / 100 * 360;
+    let change_deg = Math.abs(new_deg - remain_deg) //本次需要变化的角度
 
-          _this.setData({
-            left_circle_transform: 'rotate(' + deg + 'deg)',
-            left_circle_transition: 'all ' + time + 's linear'
-          })
-        }, Math.floor(time * 1000));
-      } else {
-        //设置右边的进度
-        time = Math.abs(curPercent - oldPercent) / 50 * speed;
-        deg = curPercent / 50 * 180 - 135;
+    if (new_deg <= 180) {
+      /**右侧半圆变化 */
+      let left_time = 0;
+      let left_change = Math.abs(180 + old_left_deg)
+      //右侧进度条改变之前，清空左侧
+      if (old_left_deg > -180) {
+        left_time = Math.abs(50 - old_percent) / 50 * speed;
+
         _this.setData({
-          right_circle_transform: 'rotate(' + deg + 'deg)',
-          right_circle_transition: 'all ' + time + 's linear'
+          left_transform: old_left_deg - left_change,
+          left_transition: left_time
         })
       }
+      time -= left_time;
+      change_deg -= left_change;
+      // 右侧进度条增加
+      if (old_percent <= target) {
+        _this.setData({
+          right_transform: old_right_deg + change_deg,
+          right_transition: time
+        })
+      } else {
+        // 右侧进度条减小
+        setTimeout(function() {
+          _this.setData({
+            right_transform: old_right_deg - change_deg,
+            right_transition: time
+          })
+        }, left_time * 1000)
+      }
+    } else if (new_deg <= 360) {
+      /**左侧半圆变化 */
+      let right_time = 0;
+      let right_change = Math.abs(0 - old_right_deg)
+      //左侧进度条改变之前，填满右侧
+      if (old_right_deg < 0) {
+        right_time = Math.abs(50 - old_percent) / 50 * speed;
+        _this.setData({
+          right_transform: old_right_deg + right_change,
+          right_transition: right_time
+        })
+      }
+      time -= right_time;
+      if (old_percent <= target) {
+        // 左侧进度条增加 
+        let left_deg = change_deg - right_change;
+        if (change_deg >= 180) {
+          left_deg -= 180;
+        } else {
+          left_deg = old_left_deg + change_deg;
+        }
+        setTimeout(function() {
+          _this.setData({
+            left_transform: left_deg,
+            left_transition: time
+          });
+        }, right_time * 1000)
+      } else {
+        // 左侧进度条减小
+        setTimeout(function() {
+          _this.setData({
+            left_transform: old_left_deg - change_deg,
+            left_transition: time
+          });
+        }, time * 1000)
+      }
+    } else {
+      console.error('Progress over range')
     }
   },
+
   onReady: function() {
     let _this = this;
+    _this.setProgressbar(40)
     setTimeout(function() {
-      _this.setProgressbar(0, 40)
+      _this.setProgressbar(60)
       setTimeout(function() {
-        _this.setProgressbar(40, 60)
+        _this.setProgressbar(40)
         setTimeout(function() {
-          _this.setProgressbar(60, 40)
-          setTimeout(function() {
-            _this.setProgressbar(40, 0)
-          }, 1000)
-        }, 1000)
-      }, 1000)
-    }, 1000)
+          _this.setProgressbar(0)
+        }, 1500)
+      }, 1500)
+    }, 1500)
   },
 })
